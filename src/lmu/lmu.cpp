e2be5b207da56ecb02682b7ef6c048067c7e76f7
FoundIzalith <b17kaylaeva@gmail.com>
2022-09-04 12:52:24 -0400

    Added kernels and encoders

--- src/lmu/lmu.cpp
+++ src/lmu/lmu.cpp
@@ -9,6 +9,15 @@ LMUCell::LMUCell() {
     matrixB = 0;
     hiddenState = 0;
     memoryVector = 0;
+
+    encodingInput = 0;
+    encodingHidden = 0;
+    encodingMemory = 0;
+
+    kernelInput = 0;
+    kernelHidden = 0;
+    kernelMemory = 0;
+
     inputSize = 0;
     hiddenSize = 0;
     memorySize = 0;
@@ -20,6 +29,14 @@ LMUCell::LMUCell(const LMUCell& original) {
     matrixB = new arma::Mat<float>(original.matrixB);
     hiddenState = new arma::Mat<float>(original.hiddenState);
     memoryVector = new arma::Mat<float>(original.memoryVector);
+
+    encodingInput = original.encodingInput;
+    encodingHidden = original.encodingHidden;
+    encodingMemory = original.encodingMemory;
+
+    kernelInput = original.kernelInput;
+    kernelHidden = original.kernelHidden;
+    kernelMemory = original.kernelMemory;
 
     inputSize = original.inputSize;
     hiddenSize = original.hiddenSize;
@@ -36,6 +53,9 @@ LMUCell::LMUCell(int i, int h, int m, int t) {
     matrices AB = generateMatrices(memorySize, theta);
     discretizeMatrices(matrixA, matrixB, memorySize);
 
+    initEncoders();
+    initKernels();
+
     hiddenState = 0;
     memoryVector = 0;
 }
@@ -45,8 +65,35 @@ LMUCell::~LMUCell() {
     delete matrixB;
     delete hiddenState;
     delete memoryVector;
+
+    delete encodingInput;
+    delete encodingHidden;
+    delete encodingMemory;
+
+    delete kernelInput;
+    delete kernelHidden;
+    delete kernelMemory;
 }
 
-void LMUCell::processInput(const arma::Cube<float> input&) {
+void LMUCell::processInput(const arma::Mat<float> input&) {
+    //Equation 7 [1]
+    //arma::Mat<float> u = 
+}
+
+void LMUCell::initEncoders() {
+    encodingInput = new arma::Mat<float>(1, inputSize);
+    encodingHidden = new arma::Mat<float>(1, hiddenSize);
+    encodingMemory = new arma::Mat<float>(1, memorySize);
+
+    //LeCun Uniform [5]
+    //Used to initialize weights such that they are not too big but not too small 
     
+}
+
+void LMUCell::initKernels() {
+    kernelInput = new arma::Mat<float>(hiddenSize, inputSize);
+    kernelHidden = new arma::Mat<float>(hiddenSize, hiddenSize);
+    kernelMemory = new arma::Mat<float>(hiddenSize, memorySize);
+
+    //Xavier Initialization [6]
 }
