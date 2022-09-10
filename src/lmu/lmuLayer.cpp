fae4762a7b2918d53c7c8c0777c68a0b235cb0d8
FoundIzalith <b17kaylaeva@gmail.com>
2022-09-10 16:00:51 -0400

    Finished processInput

--- src/lmu/lmuLayer.cpp
+++ src/lmu/lmuLayer.cpp
@@ -21,10 +21,10 @@ LMULayer::LMULayer() {
 }
 
 LMULayer::LMULayer(const LMULayer& original) {
-    cell = new LMUCell(original.cell);
-    hiddenState = new Mat<float>(original.hiddenState);
-    memoryVector = new Mat<float>(original.memoryVector);
-    output = new Cube<float>(original.output);
+    cell = new LMUCell(*original.cell);
+    hiddenState = new arma::Mat<float>(*original.hiddenState);
+    memoryVector = new arma::Mat<float>(*original.memoryVector);
+    output = new arma::Cube<float>(*original.output);
 
     inputSize = original.inputSize;
     hiddenSize = original.hiddenSize;
@@ -52,14 +52,28 @@ LMULayer::~LMULayer() {
 }
 
 void LMULayer::processInput(const arma::Cube<float>& input) {
+    //Input x = batchSize, y = sequenceLength, z = inputSize
     batchSize = input.n_rows;
     sequenceLength = input.n_cols;
+    inputSize = input.n_slices;
 
     hiddenState = new arma::Mat<float>(batchSize, hiddenSize);
     memoryVector = new arma::Mat<float>(batchSize, memorySize);
-    output = new arma::Cube<float>(sequenceLength, batchSize, hiddenSize);
+    output = new arma::Cube<float>(batchSize, sequenceLength, hiddenSize);
+    
+    arma::Cube<float> inputT(batchSize, sequenceLength, inputSize);
     
     for(int i = 0; i < sequenceLength; i++) {
-        cell.processInput(input, hiddenState, memoryVector);
+        //Iterate through input along the sequence
+        inputT = input;
+        inputT.resize(batchSize, i, inputSize);
+        cell->processInput(inputT, *hiddenState, *memoryVector);
+
+        //Add hidden state to output, layer by layer, along the sequence
+        for(int j = 0; j < batchSize; j++) {
+            for(int k = 0; k < hiddenSize; k++) {
+                output->at(j, i, k) = hiddenState->at(j, k);
+            }
+        }
     }
 }
